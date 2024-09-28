54cd0268e5809cd66e4c38f82553159ccd1c32ab
Nikola Pavlov <nikola.pavlov01@gmail.com>
2024-09-28 02:39:41 +0200

    CSS updates and Map pin updates

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -13,10 +13,8 @@ export class MapComponent implements OnInit {
 
   // Array containing lat, lon pairs
   private markerValues = [
-    41.7104071,22.8467131, // Берово - ООУ „Дедо Иљо Малешевски“
-    41.7077826,22.8545511,
-    41.707950780384465, 22.855144797486066,
-
+    41.707950780384465, 22.855144797486066, // Берово - ООУ „Дедо Иљо Малешевски“
+    41.7104071,22.8467131,
   ];
 
   // Initialize the map
@@ -39,13 +37,17 @@ export class MapComponent implements OnInit {
 
   // Function to add multiple markers to the map
   private addMarkers(): void {
+
     const defaultIconWithoutShadow = L.icon({
-      iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
-      iconSize: [25, 41],  // Size of the icon
+      // iconUrl: 'https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png',
+      iconUrl: "../assets/pngwing.com.png",
+      iconSize: [50, 41],  // Size of the icon
       iconAnchor: [12, 41],
       popupAnchor: [1, -34],
       tooltipAnchor: [16, -28],
-      shadowUrl: ''  // Remove shadow
+      // riseOnHover: true,
+      shadowUrl: '',  // Remove shadow
+      
     });
 
     // Loop through the markerValues array, adding markers in pairs (lat, lon)
@@ -55,8 +57,35 @@ export class MapComponent implements OnInit {
 
       // Create a marker at the given lat, lon
       const marker = L.marker([lat, lon], { icon: defaultIconWithoutShadow });
+      marker.on('click',(e: any) => {  // Use an arrow function to retain 'this'
+        const popLocation = new L.LatLng(lat, lon);
+        const popup = L.popup({
+          autoClose: true,
+          closeOnClick: true,
+  
+        })
+          .setLatLng(popLocation)
+          .setContent('<p>Берово - ООУ „Дедо Иљо Малешевски</p>')
+          .openOn(this.map);  // 'this' refers to the MapComponent's map instance
+      })
+  
         marker.addTo(this.map);
     }
+
+
+    const marker = L.marker([41.70654945554364, 22.853571009575017], { icon: defaultIconWithoutShadow });
+
+    marker.on('click',(e: any) => {  // Use an arrow function to retain 'this'
+
+      // ACTIVATE MODAL ON CLICK ON MARKER!!!!!!!!!
+
+      
+//       data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+// aria-controls="offcanvasRight"
+    })
+
+    marker.addTo(this.map);
+    
   }
 
   constructor() { }
